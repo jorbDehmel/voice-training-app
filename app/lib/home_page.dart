275d2c2bce6a36fbd6440cdba8a5d13243fc6028
Evelyn Drollinger-Smith <the.omniscient.q@gmail.com>
2024-12-08 14:34:29 -0700

    Polishing the UI

--- app/lib/home_page.dart
+++ app/lib/home_page.dart
@@ -8,49 +8,55 @@ import './analysis_page.dart';
 import './passthrough_page.dart';
 import './info_page.dart';
 
-class VoiceAppHomePage extends StatelessWidget {
-  const VoiceAppHomePage({super.key, required this.title});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
   final String title;
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectionID = 0;
+
+  static const List<Widget> _subscreens = <Widget>[
+    Center(child: AnalysisPage()),
+    Center(child: PassthroughPage()),
+    Center(child: InfoPage()),
+  ];
+
+  void _onMenuClick(int selection) {
+    setState(() {
+      _selectionID = selection;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        // Navigation column
-        child: Column(children: [
-          // Title
-          const Text('Voice Training App'),
-          // Jump to analysis page
-          ElevatedButton(
-            child: const Text('Analysis'),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const AnalysisPage()),
-              );
-            },
+      appBar: AppBar(
+        title: Text('Voice Training'),
+      ),
+      // Main content window: changes based on the selected index
+      body: _subscreens[_selectionID],
+
+      // Bottom navigation bar
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Analysis',
           ),
-          // Jump to passthrough page
-          ElevatedButton(
-            child: const Text('Passthrough'),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => const PassthroughPage()),
-              );
-            },
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Passthrough',
           ),
-          ElevatedButton(
-            child: const Text('Info'),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const InfoPage()),
-              );
-            },
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Info',
           ),
-        ]),
+        ],
+        currentIndex: _selectionID,
+        selectedItemColor: Colors.blue,
+        onTap: _onMenuClick,
       ),
     );
   }
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -23,7 +23,7 @@ class VoiceApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const VoiceAppHomePage(title: 'Voice App Home Page'),
+      home: const HomePage(title: 'Voice App Home Page'),
     );
   }
 }
